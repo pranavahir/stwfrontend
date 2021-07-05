a344d224823123d5403149cd4d7958ad77e37463
shoptheWorld <71921270+shoptheWorld@users.noreply.github.com>
2021-07-05 10:49:13 +0530

    api changes

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const { withPlugins } = require('next-compose-plugins');
 
 const nextConfig = {
   env: {
- API_URL: "http://localhost:4000/graphql"  //local stwapi
+//  API_URL: "http://localhost:4000/api/graphql"  //local stwapi
 //  API_URL: "http://162.0.210.5:4457api/graphql"  //local stwapi
  
 // API_URL: "https://cryptic-mountain-48315.herokuapp.com/api/graphql"  //Test env
   // API_URL:'https://stwecommerceapi.digitechniq.in/api/graphql', //production env
-  // API_URL:'https://docker.digitechniq.in/api/graphql', //production env
+  API_URL:'https://docker.digitechniq.in/api/graphql', //production env
   // docker.digitechniq.in
 
     
