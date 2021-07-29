1e1afd2bf965e3e8e3b9ec4cac22dc1b97c20172
shoptheWorld <71921270+shoptheWorld@users.noreply.github.com>
2021-07-29 10:14:15 +0530

    updated register screen for email validation
    email

--- pages/page/account/register.js
+++ pages/page/account/register.js
@@ -30,9 +30,28 @@ const Register = () => {
 
   const emailLinkSend = (email) => {
     // [START auth_email_link_send]
+
+    var actionCodeSettings = {
+      // URL you want to redirect back to. The domain (www.example.com) for this
+      // URL must be in the authorized domains list in the Firebase Console.
+      url: "https://shoptheworld.in/",
+      // This must be true.
+      handleCodeInApp: true,
+      iOS: {
+        bundleId: 'com.example.ios'
+      },
+      android: {
+        packageName: 'com.example.android',
+        installApp: true,
+        minimumVersion: '12'
+      },
+      dynamicLinkDomain: 'example.page.link'
+    };
+
+    
     firebase
       .auth()
-      .sendSignInLinkToEmail(email)
+      .sendSignInLinkToEmail(email,actionCodeSettings)
       .then(() => {
         // The link was successfully sent. Inform the user.
         // Save the email locally so you don't need to ask the user for it again
