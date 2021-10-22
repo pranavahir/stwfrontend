97bb396b8614b170e88695411825d590d91c649c
shoptheWorld <71921270+shoptheWorld@users.noreply.github.com>
2021-10-22 21:01:08 +0530

    Build Fixed

--- pages/page/search.js
+++ pages/page/search.js
@@ -2,13 +2,13 @@ import React from 'react';
 import CommonLayout from '../../components/shop/common-layout';
 import { Container, Row, Col, Input } from 'reactstrap';
 import { withApollo } from '../../helpers/apollo/apollo';
-import SelectSearch from 'react-select-search'
-const options = [{name:'Electronics',value:'et'},{name:'Fashion',value:'fs'}]
-import './style.css'
+// import SelectSearch from 'react-select-search'
+// const options = [{name:'Electronics',value:'et'},{name:'Fashion',value:'fs'}]
+// import './style.css'
 const Search = () => {
     return (
         <CommonLayout parent="home" title="search">
-                            <SelectSearch class='select-search' options={options} value="et" name="language" placeholder="All" />
+                            {/* <SelectSearch class='select-search' options={options} value="et" name="language" placeholder="All" /> */}
             {/* <!--section start--> */}
             <section className="authentication-page section-b-space">
                 <Container>
